5777c39efea22587b40c686feaf9949fbdb6eac3
AnaM93 <ana.18_03@yahoo.com>
2023-10-12 14:09:16 -0700

    created more components on the App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react'
 import ParticlesBg from 'particles-bg'; 
 import Clarifai from 'clarifai';
 import Navigation from './Components/Navigation/Navigation';
+import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
@@ -55,9 +57,30 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl : ''
+      imageUrl: '',
+      box: {},
+      route: 'signIn',
+      isSignedIn: false
     }
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({box:box})
+  }
+
   
   onInputChange = (event) => {
     this.setState({input: event.target.value})
@@ -65,38 +88,45 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
+    fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequest(this.state.input))
+      .then(response => response.json())
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  };
 
-
-  /*app.models.predict('a403429f2ddf4b49b307e318f00e528b','https://samples.clarifai.com/face-det.jpg')*/
-  fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequest(this.state.input))
-    .then(response => response.json())
-    .then(
-      function (response){
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-    },
-    function (err){
-
-    });
+onRouteChange = (route) => {
+  if (route === 'signout'){
+  this.setState({isSignedIn:false})}
+  else if (route === 'home'){
+  this.setState({isSignedIn:true})
   }
-
-
+  this.setState({route: route})
+}
   render () {
     return (
     <div className="App">
       <>
-        <div>...</div>
-        <ParticlesBg type="color" num={100} color ='white' bg={true} />
+      <ParticlesBg type="square" num={100} color ='white' bg={true} />
       </>
-      <Navigation />
-      <Logo />
-      <Rank/>
-      <ImageLinkForm 
-        onInputChange = {this.onInputChange} 
-        onButtonSubmit={this.onButtonSubmit}
-      />
-      <FaceRecognition imageUrl={this.state.imageUrl}/>
+     <Navigation isSignedIn={this.state.isSignedIn}onRouteChange = {this.onRouteChange}/>
+     {this.state.route === 'home' ?
+          <div>
+          <Logo />
+          <Rank/>
+          <ImageLinkForm 
+            onInputChange = {this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}/>
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+          </div>
+          :(
+            this.state.route === 'signIn' ?
+            <SignIn onRouteChange = {this.onRouteChange}/> :
+            <Register onRouteChange = {this.onRouteChange}/>
+          )
+          };
     </div>
   );
 }
+
 }
 export default App;
